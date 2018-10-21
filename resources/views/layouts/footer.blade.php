dbdb33e5b31123e224894ecc5ca02ef536c42c2e
Ricardo Lucas <lucasricardo28@gmail.com>
2018-10-21 17:09:38 -0300

    layout

--- resources/views/layouts/footer.blade.php
+++
@@ -1,6 +0,0 @@
-
-<footer class="main-footer">
-    Version 1.0.0.2 | Data última atualização:
-    Copyright &copy; {!! \Carbon\Carbon::now()->format("Y")!!}. Powered by.
-    <h3>Ricardo Lucas</h3>
-</footer>
